f1cd2665aac0bccb729923d41927d5b9615bdccd
ANEUP002 <146136473+ANEUP002@users.noreply.github.com>
2024-02-13 15:05:54 -0500

    playing

---
+++ my-app/public/index.js
@@ -0,0 +1,5 @@
+function Changetext(){
+    var newtext = "Gymbros";
+    document.getElementById("header").innerHTML= "Welcome to the "+ newtext;
+    window.alert("Do you want a different username??")
+}
