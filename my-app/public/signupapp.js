d8b204ec1de08f8f2fa73c770cfb67a02113e9d8
svish001 <sumeetbabelalv@gmail.com>
2024-03-09 01:38:41 -0500

    somethings missing

--- my-app/public/signupapp.js
+++ my-app/public/signupapp.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-  import { getAuth, createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
