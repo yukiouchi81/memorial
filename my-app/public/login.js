1bbbf6d07405824ef0c1e7f1087d8457968e6f42
svish001 <sumeetbabelalv@gmail.com>
2024-03-08 23:22:29 -0500

    login.js added

---
+++ my-app/public/login.js
@@ -0,0 +1,52 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBxuHJPQsKTzumuU-XPeUeG-zLTZug3F1M",
+    authDomain: "memorialgym-f5ce3.firebaseapp.com",
+    databaseURL: "https://memorialgym-f5ce3-default-rtdb.firebaseio.com",
+    projectId: "memorialgym-f5ce3",
+    storageBucket: "memorialgym-f5ce3.appspot.com",
+    messagingSenderId: "567780843830",
+    appId: "1:567780843830:web:c9c04ad30289df71d91397"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+window.login= function(e) {
+  e.preventDefault();
+  var obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  signInWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function (success) {
+      alert("logined Successfully")
+      var aaaa =  (success.user.uid);
+      localStorage.setItem("uid",aaaa)
+      console.log(aaaa)
+      
+      
+      
+      window.location.replace('')
+     // localStorage.setItem(success,user,uid)
+      
+    })
+    .catch(function (err) {
+      alert("login error"+err);
+    });
+
+  console.log(obj);
+}
